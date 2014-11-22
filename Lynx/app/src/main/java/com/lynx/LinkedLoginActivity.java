709789ac1f5feca09f012ff33220a9550029c788
Vince Lugli <vince.lugli@gmail.com>
2014-11-22 01:32:20 -0500

    Added registration logic. Registration actually creates a new user now. Also split up registration activity, one for Lynx registration, one for other accounts

---
+++ Lynx/app/src/main/java/com/lynx/LinkedLoginActivity.java
@@ -0,0 +1,217 @@
+package com.lynx;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.app.ActionBar.Tab;
+
+
+public class LinkedLoginActivity extends Activity implements ActionBar.TabListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linked_login);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(
+                new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        // Select tab when swiping.
+                        getActionBar().setSelectedNavigationItem(position);
+                    }
+                }
+        );
+
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Facebook")
+                        .setTabListener(this)
+        );
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("LinkedIn")
+                        .setTabListener(this)
+        );
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Google+")
+                        .setTabListener(this)
+        );
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Twitter")
+                        .setTabListener(this)
+        );
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Go")
+                        .setTabListener(this)
+        );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.linked_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return FacebookFragment.newInstance(position);
+                case 1:
+                    return LinkedInFragment.newInstance(position);
+                case 2:
+                    return GoogleFragment.newInstance(position);
+                case 3:
+                    return TwitterFragment.newInstance(position);
+                case 4:
+                    return GoFragment.newInstance(position);
+            }
+            return FacebookFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 6 total pages.
+            return 6;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section4).toUpperCase(l);
+                case 3:
+                    return getString(R.string.title_section5).toUpperCase(l);
+                case 4:
+                    return getString(R.string.title_section6).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_linked_login, container, false);
+            return rootView;
+        }
+    }
+
+}
--- Lynx/app/src/main/java/com/lynx/RegisterActivity.java
+++ Lynx/app/src/main/java/com/lynx/RegisterActivity.java
@@ -1,62 +1,39 @@
 package com.lynx;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.app.ActionBar.Tab;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RegisterActivity extends Activity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
 
     private EditText mPasswordView;
     private EditText mUsernameView;
     private EditText mDisplayNameView;
     private EditText mPasswordConfirmView;
-    private View mProgressView;
-    private View mRegisterFormView;
 
-    private UserRegisterTask mRegisterTask = null;
+    private View mRegisterFormView;
+    private View mProgressView;
+
+    private UserRegisterTask mRegistrationTask = null;
 
     private JSONParser jsonParser = new JSONParser();
 
@@ -64,84 +41,6 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        mViewPager.setOnPageChangeListener(
-                new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        // Select tab when swiping.
-                        getActionBar().setSelectedNavigationItem(position);
-                    }
-                }
-        );
-
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-
-            @Override
-            public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-                // TODO Auto-generated method stub
-                // show the given tab
-            }
-
-            @Override
-            public void onTabSelected(Tab tab, FragmentTransaction ft) {
-                // TODO Auto-generated method stub
-                // hide the given tab
-                if (tab.getPosition() == 0) {
-                    attemptRegister();
-                }
-                mViewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabReselected(Tab tab, FragmentTransaction ft) {
-                // TODO Auto-generated method stub
-                // probably ignore this event
-            }
-        };
-
-        actionBar.addTab(
-                actionBar.newTab()
-                        .setText("Register")
-                        .setTabListener(tabListener)
-        );
-        actionBar.addTab(
-                actionBar.newTab()
-                        .setText("Facebook")
-                        .setTabListener(tabListener)
-        );
-        actionBar.addTab(
-                actionBar.newTab()
-                        .setText("LinkedIn")
-                        .setTabListener(tabListener)
-        );
-        actionBar.addTab(
-                actionBar.newTab()
-                        .setText("Google+")
-                        .setTabListener(tabListener)
-        );
-        actionBar.addTab(
-                actionBar.newTab()
-                        .setText("Twitter")
-                        .setTabListener(tabListener)
-        );
-        actionBar.addTab(
-                actionBar.newTab()
-                        .setText("Go")
-                        .setTabListener(tabListener)
-        );
-
 
         mPasswordView = (EditText) findViewById(R.id.password_edit_text);
         mUsernameView = (EditText) findViewById(R.id.username_edit_text);
@@ -152,8 +51,8 @@ public class RegisterActivity extends Activity {
         mProgressView = findViewById(R.id.register_progress);
     }
 
-    public void attemptRegister() {
-        if (mRegisterTask != null) {
+    public void attemptRegister(View view) {
+        if(mRegistrationTask != null) {
             return;
         }
 
@@ -173,6 +72,7 @@ public class RegisterActivity extends Activity {
         // Check for valid username
         if (TextUtils.isEmpty(username)){
             mUsernameView.setError(getString(R.string.error_field_required));
+            cancel = true;
         }
         else if (!isUsernameValid(username)) {
             mUsernameView.setError(getString(R.string.error_invalid_username));
@@ -183,9 +83,10 @@ public class RegisterActivity extends Activity {
         // Check for valid displayName
         if (TextUtils.isEmpty(displayName)){
             mDisplayNameView.setError(getString(R.string.error_field_required));
+            cancel = true;
         }
         else if (!isUsernameValid(displayName)) {
-            mDisplayNameView.setError(getString(R.string.error_invalid_username));
+            mDisplayNameView.setError(getString(R.string.error_invalid_displayname));
             focusView = mDisplayNameView;
             cancel = true;
         }
@@ -193,17 +94,29 @@ public class RegisterActivity extends Activity {
         // Check if password and passwordConfirm Match
         if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_field_required));
+            cancel = true;
         }
 
         if (TextUtils.isEmpty(passwordConfirm)) {
             mPasswordConfirmView.setError(getString(R.string.error_field_required));
+            cancel = true;
         }
         else if (!TextUtils.isEmpty(password) && !TextUtils.isEmpty(passwordConfirm)) {
-            if (TextUtils.equals(password, passwordConfirm)) {
-                mPasswordView.setError(getString(R.string.error_invalid_password));
+            if (!TextUtils.equals(password, passwordConfirm)) {
+                mPasswordView.setError(getString(R.string.error_not_matching_passwords));
                 focusView = mPasswordView;
                 cancel = true;
             }
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mRegistrationTask = new UserRegisterTask(username, displayName, password);
+            mRegistrationTask.execute((Void) null);
         }
     }
 
@@ -213,179 +126,6 @@ public class RegisterActivity extends Activity {
 
     private boolean isPasswordValid(String password) {
         return password.length() < 24;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.register, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return RegisterFragment.newInstance(position);
-                case 1:
-                    return FacebookFragment.newInstance(position);
-                case 2:
-                    return LinkedInFragment.newInstance(position);
-                case 3:
-                    return GoogleFragment.newInstance(position);
-                case 4:
-                    return TwitterFragment.newInstance(position);
-                case 5:
-                    return GoFragment.newInstance(position);
-            }
-            return RegisterFragment.newInstance(position);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 6 total pages.
-            return 6;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-                case 3:
-                    return getString(R.string.title_section3).toUpperCase(l);
-                case 4:
-                    return getString(R.string.title_section3).toUpperCase(l);
-                case 5:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-    }
-
-
-    public static class RegisterFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section number.
-         */
-        public static RegisterFragment newInstance(int sectionNumber) {
-            RegisterFragment fragment = new RegisterFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public RegisterFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_register,
-                    container, false);
-            return rootView;
-        }
-    }
-
-    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mUsername;
-        private final String mDisplayName;
-        private final String mPassword;
-        private final String mPasswordConfirm;
-
-        UserRegisterTask (String username, String displayName, String password, String passwordConfirm) {
-            mUsername = username;
-            mDisplayName = displayName;
-            mPassword = password;
-            mPasswordConfirm = passwordConfirm;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-
-            String url = "http://143.215.109.184/t4j/.php";
-            System.out.println(url);
-            List<NameValuePair> requestParams = new ArrayList<NameValuePair>();
-
-            JSONObject json = jsonParser.makeHttpRequest(url, "POST", requestParams);
-
-            try {
-                int success = json.getInt("success");
-                if (success == 1) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mRegisterTask = null;
-            showProgress(false);
-
-            if (success) {
-                Intent lynxIntent = new Intent (getApplicationContext(), LynxActivity.class);
-                startActivity(lynxIntent);
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mRegisterTask = null;
-            showProgress(false);
-        }
-    }
-
-    public void goToLynx(View view) {
-        Intent lynxIntent = new Intent(this, LynxActivity.class);
-
-        startActivity(lynxIntent);
     }
 
     /**
@@ -421,6 +161,80 @@ public class RegisterActivity extends Activity {
             // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mRegisterFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mUsername;
+        private final String mDisplayName;
+        private final String mPassword;
+
+        UserRegisterTask (String username, String displayName, String password) {
+            mUsername = username;
+            mDisplayName = displayName;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            String url = "http://143.215.109.184/t4j/create_user.php";
+            System.out.println(url);
+            List<NameValuePair> requestParams = new ArrayList<NameValuePair>();
+            requestParams.add(new BasicNameValuePair("username", mUsername));
+            requestParams.add(new BasicNameValuePair("password", mPassword));
+            requestParams.add(new BasicNameValuePair("displayName", mDisplayName));
+
+            JSONObject json = jsonParser.makeHttpRequest(url, "POST", requestParams);
+
+            try {
+                int success = json.getInt("success");
+                return success == 1;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mRegistrationTask = null;
+            showProgress(false);
+
+            if (success) {
+                Intent lynxIntent = new Intent (getApplicationContext(), LynxActivity.class);
+                startActivity(lynxIntent);
+                finish();
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mRegistrationTask = null;
+            showProgress(false);
         }
     }
 }
