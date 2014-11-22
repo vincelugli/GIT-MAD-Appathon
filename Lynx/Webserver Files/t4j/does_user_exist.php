059aaebe232e4fa5ce4630848cce03bd285ca848
comfybunny <yanxialin2001@yahoo.com>
2014-11-21 21:01:53 -0500

    webserver files
    for reference

---
+++ Lynx/Webserver Files/t4j/does_user_exist.php
@@ -0,0 +1,68 @@
+<?php
+ 
+/*
+ * Following code will get single product details
+ * A product is identified by product id (pid)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+ 
+// check for post data
+if (isset($_GET["username"])) {
+    $username = $_GET['username'];
+ 
+    // get a product from products table
+    // SELECT * FROM `users` WHERE username = "test"
+    // "SELECT *FROM users WHERE username = $username"
+    $result = mysql_query("SELECT username FROM `users` WHERE username = \"$username\"");
+ 
+    if (!empty($result)) {
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+ 
+            $result = mysql_fetch_array($result);
+ 
+            $user = array();
+            $user["username"] = $result["username"];
+            // success
+            $response["success"] = 1;
+ 
+            // user node
+            $response["user"] = array();
+ 
+            array_push($response["user"], $user);
+ 
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no user found
+            $response["success"] = 0;
+            $response["message"] = "No user FAIL found";
+ 
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no user found
+        $response["success"] = 0;
+        $response["message"] = "No user found";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
